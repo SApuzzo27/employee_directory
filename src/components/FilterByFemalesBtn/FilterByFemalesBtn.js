b6ee3d30ad89194b92cf6cf12ffdd8c729792d15
Sam <SApuzzo527@gmail.com>
2020-12-21 17:01:36 -0500

    corrected btn name

--- src/components/FilterByFemalesBtn/FilterByFemalesBtn.js
+++ src/components/FilterByFemalesBtn/FilterByFemalesBtn.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 
-function AgeBtn (props) {
+function FemalesBtn (props) {
     return (<div>
         <Button style={{width: "100%"}} variant="dark" onClick={props.filterFemales}>Filter By Females!</Button>{' '}
     </div>)
 }
 
-export default AgeBtn;
+export default FemalesBtn;
--- src/components/FilterByMalesBtn/FilterByMalesBtn.js
+++ src/components/FilterByMalesBtn/FilterByMalesBtn.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 
-function AgeBtn (props) {
+function MalesBtn (props) {
     return (<div>
         <Button style={{width: "100%"}} variant="dark" onClick={props.filterMales}>Filter By Males!</Button>{' '}
     </div>)
 }
 
-export default AgeBtn;
+export default MalesBtn;
