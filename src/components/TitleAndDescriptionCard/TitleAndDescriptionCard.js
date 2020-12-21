87b2a4c2ae198f77cdc951f66ee46e29878d2ac0
Sam <SApuzzo527@gmail.com>
2020-12-21 17:38:48 -0500

    adjusted verbage title descript

--- src/components/TitleAndDescriptionCard/TitleAndDescriptionCard.js
+++ src/components/TitleAndDescriptionCard/TitleAndDescriptionCard.js
@@ -15,7 +15,7 @@ return ( <Container>
                     <h1 style={{textAlign:"center", fontSize:"50px", color:"darkblue"}}>Employee Directory</h1>
                     </Col>
                     <Col size="md-12">
-                    <p style={{textAlign:"center"}}>Use this App to Sort your Employees by age or for filter by Male or Female!</p>
+                    <p style={{textAlign:"center"}}>Use this App to Sort your Employees by age or to filter by Male/Female!</p>
                     </Col>
                 </Row>
             </Card>
